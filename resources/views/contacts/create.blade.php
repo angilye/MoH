0ca1aeeddc19a7ead5dadfdf1235d4e19b7fd0cb
angilye <ange.jacques@outlook.fr>
2018-01-07 11:24:48 +0100

    ajout de champs dans le formulaire contact

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -20,6 +20,53 @@
                     </div>
 
                     <div class="form-group">
+                        <label for="raison" class="control-label">Raison :</label><br />
+                        <select name="raison" id="raison" class="form-control" required="required">
+                            <optgroup label="Site">
+                            <option value="navigation">Navigation</option>
+                            <option value="inscription">Inscription</option>
+                            <option value="connexion">Connexion</option>
+                            <optgroup label="Serveur">
+                            <option value="Glitch">Glitch</option>
+                            <option value="bug">Bug</option>
+                            <option value="connexion">Rejoindre Serveur</option>
+                        </select>
+                    </div>
+
+                    <fieldset class="form-group">
+                        <div class="row">
+                            <legend class="col-form-label col-sm-2 pt-0">Importance:</legend>
+                            <div class="col-sm-10">
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                    <label class="form-check-label" for="inlineRadio1">1</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                    <label class="form-check-label" for="inlineRadio2">2</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                                    <label class="form-check-label" for="inlineRadio3">3</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio4" value="option4">
+                                    <label class="form-check-label" for="inlineRadio4">4</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio5" value="option5">
+                                    <label class="form-check-label" for="inlineRadio5">5</label>
+                                </div>
+                            </div>
+                        </div>
+                    </fieldset>
+
+                    <div class="form-group">
+                        <label for="url" class="control-label">URL:</label>
+                        <input type="url" name="url" id="url" class="form-control" required="required">
+                    </div>
+                    
+                    <div class="form-group">
                         <label for="message" class="control-label sr-only">Message</label>
                         <textarea name="message" id="message" class="form-control" cols="30" rows="10" required="required"></textarea>
                     </div>
