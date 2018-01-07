6caa890105ad0688f0d4175602cb69a7abb519b6
angilye <ange.jacques@outlook.fr>
2018-01-07 11:26:21 +0100

    Modification page acceuil et html lang = fr

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,37 +4,37 @@
 <div class="container">
     <div class="body">
  
-            <div id="fond_acceuil">
-                <div id="acceuil_text">
-                    <div id="text_acceuil">
-                        <p class="text-center">
-                            <span class="taille60">Site en construction </span> 
-                            <br>
-                            <span class="taille28">(21/12/2017)</span> 
-                            <br>
-                            <span class="taille40">le site sera disponible tr&#232;s bient&#244;t !</span> 
-                            <br>
-                            <span class="taille28" >(vous pouvez quand m&#234;me acc&#233;der &#224; certaine fonctionnalit&#233;s)</span> 
-                            <br>
-                        </p>
-                    </div>
-                    <div id="separateur_acceuil" class="text-center">
-                        &#126;
-                    </div>
-                    <div id="text_acceuil_en">
-                        <p class="text-center">
-                            <span class="taille60">Under Construction</span> 
-                            <br>
-                            <span class="taille28">(21/12/2017)</span> 
-                            <br>
-                            <span class="taille40">This website will be available soon!</span> 
-                            <br>
-                            <span class="taille28" >(You can still access certain features)</span> 
-                            <br>
-                        </p>
-                    </div>
+        <div id="fond_acceuil">
+            <div id="acceuil_text">
+                <div id="text_acceuil">
+                    <p class="text-center">
+                        <span class="taille60">Site en construction </span> 
+                        <br>
+                        <span class="taille28">(21/12/2017)</span> 
+                        <br>
+                        <span class="taille40">le site sera disponible tr&#232;s bient&#244;t !</span> 
+                        <br>
+                        <span class="taille28" >(vous pouvez quand m&#234;me acc&#233;der &#224; certaine fonctionnalit&#233;s)</span> 
+                        <br>
+                    </p>
+                </div>
+                <div id="separateur_acceuil" class="text-center">
+                    &#126;
+                </div>
+                <div id="text_acceuil_en">
+                    <p class="text-center">
+                        <span class="taille60">Under Construction</span> 
+                        <br>
+                        <span class="taille28">(21/12/2017)</span> 
+                        <br>
+                        <span class="taille40">This website will be available soon!</span> 
+                        <br>
+                        <span class="taille28" >(You can still access certain features)</span> 
+                        <br>
+                    </p>
                 </div>
             </div>
+        </div>
    
     </div>
 </div>
